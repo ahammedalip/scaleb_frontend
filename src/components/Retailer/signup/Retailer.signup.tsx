70997c017c9fd9d32a61b96457b961b290359e3c
ahammedalip <ahammed.puthuppalli@gmail.com>
2024-02-06 17:16:51 +0530

    verify otp is almost completed

--- src/components/Retailer/signup/Retailer.signup.tsx
+++ src/components/Retailer/signup/Retailer.signup.tsx
@@ -1,7 +1,6 @@
 import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import api from '../../../axios/api.ts'
-// import './retailer.signup.css'
 
 
 interface FormData {
@@ -17,98 +16,166 @@ function Retailersignup() {
   const [formData, setFormData] = useState<FormData>({})
   const [otp, setOTP] = useState<string>('')
   const [error, setError] = useState<string>('')
+  const [isOtpSent, setIsOtpSent] = useState(false);
+  const [passwordError, setPasswordError] = useState<string>('')
+
 
   const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.id]: e.target.value })
+    setError('')
+    if (e.target.id === "password" && e.target.value.length < 6) {
+      setPasswordError('Password must contain 6 characters!')
+    } else {
+      setPasswordError('')
+    }
+
     console.log(formData);
   }
-
+  const validateFormData = () => {
+    const { retailerName, email, password } = formData;
+  
+    // Check if retailerName is empty
+    if (!retailerName || retailerName.trim() === '') {
+      setError('Retailer Name is required.');
+      return false;
+    }
+  
+    // Check if email is empty or invalid
+    if (!email || !/\S+@\S+\.\S+/.test(email)) {
+      setError('Please enter a valid email address.');
+      return false;
+    }
+  
+    // Check if password is empty or too short
+    if (!password || password.length <  6) {
+      setError('Please enter a password');
+      return false;
+    }
+  
+    // If we reach here, all fields are valid
+    setError('');
+    return true;
+  };
+  
   const handleSubmitCred = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(formData);
+    console.log('validate',validateFormData);
+    if(!validateFormData()){
+      
+      return;
+    }
     try {
-      const response = await api.post('/retailer/signup/verify_cred', formData,{
-        headers:{
+      
+      const response = await api.post('/retailer/signup/verify_cred', formData, {
+        headers: {
           "Content-Type": "application/json"
         }
       })
       const data = response.data;
-    console.log('coming from retailer signup',data)
-      
+      console.log('coming from retailer signup', data)
+      if (data.success === true && data.message == 'OTP send succesfully') {
+        setIsOtpSent(true);
+      }
+
     }
-    catch (err:any) {
+    catch (err: any) {
       console.log(err?.response.data);
       setError(err?.response.data.message || "An error occured");
     }
 
   }
-const handleOTPChange = async (e:ChangeEvent<HTMLInputElement>)=>{
-setOTP(e.target.value)
-console.log(otp);
+  const handleOTPChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    setOTP(e.target.value)
+    console.log(otp);
 
-}
-const handleSubmitOTP = async (e:FormEvent<HTMLFormElement>)=>{
-  e.preventDefault()
-  
-  console.log(otp)
-
-}
-
+  }
+  const handleSubmitOTP = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      const response =await api.post('/retailer/signup/verify_otp', otp, {
+        headers:{
+          "Content-Type": 'application/json'
+        }
+      })
+      const data = response.data;
+    } catch (error) {
+      
+    }
+    console.log(otp)
+  }
+ 
   return (
-    <div className='bg-gradient-to-r from-slate-300 to-slate-400'>
+    
+    <div className='bg-gradient-to-r from-slate-300 to-slate-400 min-h-screen'>
       {/* <div className='container'>    */}
       <div className='pt-11 flex'>
-        <div className='w-2/3 pt-20 pb-44'>
-          <div className=' flex items-center justify-center h-full'>
-            <div className=' bg-white bg-opacity-100 p-8 shadow-md rounded-tl-md rounded-bl-md w-96'>
-              <form className=' grad' onSubmit={handleSubmitCred}>
-                <h2 className='text-2xl font-bold mb-4 text-center'> Retailer SignUp</h2>
-                <div className='mb-4'>
-                  {error && (
-                  <div className="mb-4 text-red-500 text-center">{error}</div>
-                )}
-                </div>
-                <div className='mb-4'>
+        <div className=' pt-20 '>
+          <div className=' flex items-center justify-center h-full '>
+            <div className=' ml-48 bg-white  p-8 shadow-left-bottom rounded-tl-md rounded-bl-md w-96'>
+              {!isOtpSent ? (
 
 
-                  <input type="text" id='retailerName' className='w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500' placeholder='Enter your retailer name' onChange={handleChange} />
-                </div>
-                <div className='mb-4'>
-                  <input type="text" id='email' className='w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500' placeholder='Enter your email' onChange={handleChange} />
-                </div>
-                <div className='mb-4'>
-                  <input type="password" id='password' className='w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500' placeholder='Enter your password' onChange={handleChange} />
-                </div>
-                <div className='mb-4 text-center'>
-                  <button
-                    type="submit"
-                    className="w-auto px-3 text-center bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
-                  >
-                    Send OTP
-                  </button>
-                </div>
-              </form >
-              <form onSubmit={handleSubmitOTP}>
-                <div className='mb-4 flex '>
-                  <div className='flex-auto'>
-                    <input type="text" id='otp' className='w-32 px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500' placeholder='OTP' onChange={handleOTPChange}/>
+                <form className=' grad' onSubmit={handleSubmitCred}>
+                  <h2 className='text-2xl font-bold mb-4 text-center'> Retailer SignUp</h2>
+                  <div className='mb-4 pt-4'>
+                    {error && (
+                      <div className="mb-4 text-red-500 text-center">{error}</div>
+                    )}
+                  </div>
+                  <div className='mb-4'>
+
+                    <h1 className='ml-3'>Name<span className='text-red-500'>*</span>:</h1>
+                    <input type="text" id='retailerName' className='w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500' placeholder='Enter your retailer name' onChange={handleChange} />
 
                   </div>
-                  <div className='flex-auto'>
+                  <div className='mb-4'>
+                    <h1 className='ml-3'>email<span className='text-red-500'>*</span>:</h1>
+                    <input type="text" id='email' className='w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500' placeholder='Enter your email' onChange={handleChange} />
+                  </div>
+                  <div className='mb-4'>
+                    <h1 className='ml-3'>Password<span className='text-red-500'>*</span>:</h1>
+
+                    <input type="password" id='password' className='w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500' placeholder='Enter your password' onChange={handleChange} />
+                    {passwordError && <div className="text-red-500 text-xs mt-1">{passwordError}</div>}
+                  </div>
+                  <div className='mb-4 text-center'>
                     <button
                       type="submit"
-                      className="w-auto px-3 bg-green-600 text-white py-2 rounded-md hover:bg-green-700 transition duration-300"
+                      className="w-auto px-3 text-center bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300 disabled:bg-disabled disabled:text-disabled"
+                      disabled={!!passwordError}
                     >
-                      Verify and Submit
+                      Send OTP
                     </button>
                   </div>
+                </form >
+              ) : (
+                <div>
+                  <h2 className='text-2xl font-bold mb-4 text-center'>Submit OTP</h2>
+                  <form onSubmit={handleSubmitOTP}>
+                    <div className='mb-4 flex '>
+                      <div className='flex-auto'>
+                        <input type="text" id='otp' className='w-32 px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500' placeholder='OTP' onChange={handleOTPChange} />
 
+                      </div>
+                      <div className='flex-auto'>
+                        <button
+                          type="submit"
+                          className="w-auto px-3 bg-green-600 text-white py-2 rounded-md hover:bg-green-700 transition duration-300"
+                        >
+                          Verify and Submit
+                        </button>
+                      </div>
+
+                    </div>
+
+                  </form>
                 </div>
 
-              </form>
+              )}
             </div>
-            <div className='w-1/2'>
-              <img src="../../../../public/images/signupSide.jpg" alt="" />
+            <div className='shadow-bo rounded-tr-md rounded-br-md'>
+              <img src="../../../../public/images/signupSide.jpg" alt=""  className=' max-w-[444px] shadow-bottom-right rounded-tr-md rounded-br-md'/>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,17 @@ export default {
       rounded: {
         'full-tr-bl': '20% 40% 60% 10% / 70% 70% 10% 30%',
       },
+      backgroundColor: {
+        'disabled': '#cccccc', // Replace with the color you want for the disabled state
+      },
+      textColor: {
+        'disabled': '#ffffff', // Replace with the color you want for the text in the disabled state
+      },
+      // boxShadow: {
+      //   'bottom-right': '10px  10px  8px rgba(0,  0,  0,  0.25)',
+      //   'left-bottom': '-10px  10px  8px rgba(0,  0,  0,  0.25)',
+      //   // ... other custom box shadows
+      // },
     },
   },
   plugins: [],
