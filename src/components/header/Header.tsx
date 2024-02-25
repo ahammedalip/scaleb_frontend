18c0c52c06b2782f9a1c0b4ebf1c37efc7078f4b
ahammedalip <ahammed.puthuppalli@gmail.com>
2024-02-25 19:39:36 +0530

    changed shadow size in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -38,7 +38,7 @@ function Header() {
 
   return (
     <div className='flex justify-center'>
-      <div className="fixed p-4 w-[98%] border-box shadow-xl flex justify-between items-center rounded-full bg-white">
+      <div className="fixed p-4 w-[98%] border-box shadow-lg flex justify-between items-center rounded-full bg-white">
         <h2 className="headerLogo pl-5 text-xl">SCALE.B</h2>
         {isRetailerLoggedIn ? (
           <div className='flex items-center space-x-10 mr-4'>
