be405f895b2f3c83f0b1fcb7884eacb804e6c6c1
ahammedalip <ahammed.puthuppalli@gmail.com>
2024-02-26 15:14:18 +0530

    profile of production r is completed

---
+++ src/pages/Production/Profile/ProfilePage.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from '../../../components/header/Header';
+import ProfileProd from '../../../components/Production/Profile/Profile.Prod';
+import ProductionMenu from '../../../components/Production/Menu/ProductionMenu';
+import AdditionalMenu from '../../../components/Production/AdditionalMenu/AdditionalMenu';
+
+function ProfilePage() {
+    return (
+        <div>
+            <Header />
+            <div className='bg-red-50/40 min-h-screen pt-20 flex space-x-5'>
+                <div className='w-1/5'>
+                    <div className='space-y-5'>
+                        <ProductionMenu />
+                        <AdditionalMenu/>
+                    </div>
+                </div>
+                
+                <div className='w-8/12 h-screen'>
+                   
+
+                    <ProfileProd />
+                
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProfilePage;
