e198e5019b5aa60b3a41020197c487b64c0f75ff
ahammedalip <ahammed.puthuppalli@gmail.com>
2024-04-14 16:41:32 +0530

    socket url test-1

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from 'socket.io-client'
 
 
-const socket: Socket = io(import.meta.env.VITE_SOCKET_URL)
+const socket: Socket = io(import.meta.env.VITE_BASE_URL)
 
 
 const sendMessage = (message: string) => {
