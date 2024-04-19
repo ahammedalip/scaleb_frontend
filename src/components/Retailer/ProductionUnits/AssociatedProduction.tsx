d210ee13bcff552fffd04707ab553c95bcc3279e
ahammedalip <ahammed.puthuppalli@gmail.com>
2024-04-19 10:30:07 +0530

    used the loading feature

--- src/components/Retailer/ProductionUnits/AssociatedProduction.tsx
+++ src/components/Retailer/ProductionUnits/AssociatedProduction.tsx
@@ -3,6 +3,7 @@ import api from '../../../axios/api';
 import { useNavigate } from 'react-router-dom';
 import img from '../../../../public/images/Product_pro.png'
 import toast from 'react-hot-toast';
+import ClipLoader from 'react-spinners/ClipLoader'
 
 function ProductionConnected() {
   const [ProductionList, setProductionList] = useState([]);
@@ -38,24 +39,32 @@ function ProductionConnected() {
       <div className='text-center'>
         <h1 className='heading1 font-bold ' style={{ fontSize: '20px', textShadow: '1px 3px 4px grey' }}>Connected Production Units</h1>
       </div>
+      {loading ? (
+        <div className='flex items-center justify-center text-center h-40'>
+          <ClipLoader />
+        </div>
+      ) : (
 
-      <div className='profile text-center p-5 flex flex-col md:flex-row'>
-        {ProductionList.map((unit: {
-          _id: number,
-          name: string,
-          productionName: string
-        }, index) => (
-          <div key={index} className='flex flex-col items-center justify-center p-3'>
-            <img src={img} alt={unit.name} className='w-24 rounded-md text-center border-gray-400 shadow-gray-400 shadow-md' />
-            <div>
-              <h2>{unit.productionName}</h2>
+
+
+        <div className='profile text-center p-5 flex flex-col md:flex-row'>
+          {ProductionList.map((unit: {
+            _id: number,
+            name: string,
+            productionName: string
+          }, index) => (
+            <div key={index} className='flex flex-col items-center justify-center p-3'>
+              <img src={img} alt={unit.name} className='w-24 rounded-md text-center border-gray-400 shadow-gray-400 shadow-md' />
+              <div>
+                <h2>{unit.productionName}</h2>
+              </div>
+              <div className='p-1'>
+                <button className='bg-pink-500/85 hover:bg-pink-700 hover:text-white p-2  rounded-md shadow-md' onClick={() => handleViewClick(unit._id)}>View</button>
+              </div>
             </div>
-            <div className='p-1'>
-              <button className='bg-pink-500/85 hover:bg-pink-700 hover:text-white p-2  rounded-md shadow-md' onClick={() => handleViewClick(unit._id)}>View</button>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
       <div className='text-center justify-center flex space-x-5 pt-5'>
         <button disabled
           className="border rounded-full p-2 shadow-sm disabled:bg-white disabled:text-gray-500 disabled:cursor-not-allowed enabled:hover:bg-black enabled:hover:text-white"
